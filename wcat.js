20e0153f46088f787507ebfa438f918f3ee35752
mohammadparvez921 <parrymd786@gmail.com>
2022-03-30 03:23:54 +0530

    :fire: wcat project completed

--- wcat.js
+++ wcat.js
@@ -2,9 +2,15 @@ const fs=require('fs');
 let inputArr=process.argv.slice(2);
  console.log(inputArr);
 let filesArr=[];
-//placed files path in filesArr
+let optionsArr=[];
+//placed files path in filesArr and optionsArr
 for(let i=0;i<inputArr.length;i++){
+    let firstChar=inputArr[i].charAt(0);
+    if(firstChar=='-'){
+        optionsArr.push(inputArr[i]);
+    }else{
     filesArr.push(inputArr[i]);
+}
 }
 // console.log("file to be read are"+ filesArr);
 //check if all the files are present
@@ -19,6 +25,83 @@ for(let i=0;i<filesArr.length;i++){
 let content="";
 for (let i = 0; i < filesArr.length; i++) {
     let fileContent=fs.readFileSync(filesArr[i]);
-    content+=fileContent+"\n";
+    content=content+fileContent+"\r\n";
 }
-console.log(content);
+console.log(content+" ");
+
+//now our third command -s which will delete all extra lines from the file
+let contentArr=content.split("\r\n")
+
+//check if -s is present or not
+let tempArr=[];
+let isSPresent=optionsArr.includes("-s");
+if(isSPresent){
+  for(let i=1;i<contentArr.length;i++){
+      if(contentArr[i]=="" && contentArr[i-1]==""){
+          contentArr[i]=null;
+      }
+      else if(contentArr[i]=="" && contentArr[i-1]==null){
+          contentArr[i]=null;
+      }
+  } 
+  
+
+  //pushing everything in tempArr except null
+  for(let i=0;i<contentArr.length;i++){
+      if(contentArr[i]!=null){
+          tempArr.push(contentArr[i]);
+      }
+  }
+  contentArr=tempArr;
+}
+
+
+let indexofN=optionsArr.indexOf("-n");
+let indexofB=optionsArr.indexOf("-b");
+//if both -n and -b are not present,-1 is returned
+
+let finalOption="";
+//if both -n and -b are present
+if(indexofN!=-1 && indexofB!=-1){
+if(indexofN<indexofB){
+    finalOption="-n";
+}
+else{
+    finalOption="-b";
+}
+}
+//either -n is present or -b is present
+else{
+    if(indexofN!=-1){
+        finalOption="-n";
+    }
+    else if(indexofB!=-1){
+        finalOption="-b";
+    }
+}
+
+//calling of functions by evaluating finalOption
+if(finalOption=="-n"){
+    modifyContentByN();
+}
+else if(finalOption=="-b"){
+    modifyContentByB();
+}
+
+function modifyContentByN(){
+    for(let i=0;i<contentArr.length;i++){
+        contentArr[i]=(i+1) +")"+contentArr[i];
+    }
+}
+
+function modifyContentByB(){
+    let count=1;
+    for(let i=0;i<contentArr.length;i++){
+        if(contentArr[i]!=""){
+            contentArr[i]=count+")"+contentArr[i];
+            count++;
+        }
+    }
+}
+ 
+console.log(contentArr);
